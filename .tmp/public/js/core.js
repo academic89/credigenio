e11299eb8f8f6be1f8c870415cd354387a26c86a
Christian <csm.design89@gmail.com>
2019-12-05 20:38:21 -0600

    Auto stash before rebase of "origin/master"

--- .tmp/public/js/core.js
+++ .tmp/public/js/core.js
@@ -1,13 +1,12 @@
-'use strict';
 
-var onlyNumber = function onlyNumber() {
+var onlyNumber = function(){
     var e = document.getElementById('phone');
 
-    if (!/^[0-9]+$/.test(e.value)) {
+    if (!/^[0-9]+$/.test(e.value)) { 
         // alert("Please enter onyl number.");
-        e.value = e.value.substring(0, e.value.length - 1);
+        e.value = e.value.substring(0,e.value.length-1);
     }
-};
+}
 
 $('#money').inputmask("numeric", {
     radixPoint: ".",
@@ -16,40 +15,99 @@ $('#money').inputmask("numeric", {
     autoGroup: true,
     prefix: '$', //No Space, this will truncate the first character
     rightAlign: false,
-    oncleared: function oncleared() {
-        self.Value('');
-    }
+    oncleared: function () { self.Value(''); }
 });
 
-var createLead = function createLead() {
+$('.btn-next').click(function(){
+    let url = $(this).data('url');
+    let pathname = window.location.pathname;
 
-    $('#form-lead').submit(function (event) {
+    switch (pathname) {
+        case '/step2':
+            let check = $('input[name="radio"]').is(':checked');
+            let check2 = $('input[name="radio2"]').is(':checked');
+            if(!check) { 
+                alert("selecciona un ingreso mensual") 
+            } else if(!check2){
+                alert("selecciona si puedes comprobar tus ingresos") 
+            } else {
+                window.location.pathname = url;
+            }
+            break;
+        case '/step3':
+            let check3 = $('input[name="radio"]').is(':checked');
+            if(!check3) {
+                alert('selecciona un estatus laboral')
+            } else {
+               window.location.pathname = url; 
+            }
+            break;
+        case '/step4':
+            let check4 = $('input[name="radio"]').is(':checked');
+            if(!check4) {
+                alert('selecciona una opción')
+            } else {
+               window.location.pathname = url; 
+            }
+            break;
+        case '/step6':
+            let terms = $('input#terms').val();
+            let privacy = $('input#terms').val();
+            if(!$("#terms").prop("checked")){
+                alert('Debes aceptar los Términos y condiciones');
+            } else if(!$("#privacy").prop("checked")){
+                alert('Debes aceptar el Aviso de privacidad');
+            } else {
+                window.location.pathname = url; 
+            }
+            break;
+    }
+    
+})
+
+$('input.cbx[type=checkbox]').click(function(){
+  let valCheck = $('input.cbx[type=checkbox]').is(':checked');
+  if (valCheck) {
+      $('.hidden-box').fadeIn('slow');
+      $('#textInfo').fadeOut('slow');
+  }else{
+      $('.hidden-box').fadeOut();
+      $('#textInfo').fadeIn();
+  };
+});
+
+
+var createLead = function(){
+
+    $('#form-lead').submit(function(event){
         event.preventDefault();
 
         var date = new Date();
-        var d = date.getDate();
-        var day = d < 10 ? '0' + d : d;
+        var d  = date.getDate();
+        var day = (d < 10) ? '0' + d : d;
         var m = date.getMonth() + 1;
-        var month = m < 10 ? '0' + m : m;
+        var month = (m < 10) ? '0' + m : m;
         var yy = date.getYear();
-        var year = yy < 1000 ? yy + 1900 : yy;
-
+        var year = (yy < 1000) ? yy + 1900 : yy;
+        
         var h = date.getHours();
-        var hour = h < 10 ? '0' + h : h;
+        var hour = (h < 10) ? '0' + h : h;
         var mi = date.getMinutes();
-        var minute = mi < 10 ? '0' + mi : mi;
+        var minute = (mi < 10) ? '0' + mi : mi;
         var s = date.getSeconds();
-        var second = s < 10 ? '0' + s : s;
+        var second = (s < 10) ? '0' + s : s;
+
 
         var dateFull = day + "/" + month + "/" + year;
         var hourFull = hour + ':' + minute + ':' + second;
+
 
         var monto = $('#money').val(),
             plazo = $('#plazo').val(),
             phone = $('#phone').val(),
             email = $('#email').val(),
             account = $('input[name=cuenta]:checked').val(),
-            estatus = $('#estatus').val(),
+            estatus = $('input#estatus').val(),
             birth = $('#birth').val(),
             terms = $('input#terms').val(),
             privacy = $('input#privacy').val(),
@@ -57,77 +115,59 @@ var createLead = function createLead() {
             dateHour = hourFull,
             emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
+          
         //Validations Form
-        if (monto == '' || monto == '$') {
-            alert('Ingresa una cantidad válida');
-        } else if (plazo == null) {
-            alert('Selecciona un plazo');
-        } else if (phone == '') {
-            alert('Ingresa un teléfono válido');
-        } else if (phone.length < 10) {
-            alert('tu telefono debe ser de 10 dígitos');
-        } else if (email == '') {
-            alert('Ingresa un correo electrónico válido');
-        } else if (emailRegex.test(email) == false) {
-            alert('Ingresa un correo electrónico válido');
-        } else if (account == undefined) {
-            alert('Selecciona si cuentas o no con cuenta bancaría o de ahorro');
-        } else if (!$("#terms").prop("checked")) {
-            alert('Debes aceptar los Términos y condiciones');
-        } else if (!$("#privacy").prop("checked")) {
-            alert('Debes aceptar El aviso de privacidad');
+        if(phone == '') {
+            alert('Ingresa un teléfono válido')
+        } else if(phone.length < 10){
+            alert('tu telefono debe ser de 10 dígitos'); 
+        } else if(email == '') {
+            alert('Ingresa un correo electrónico válido')
+        } else if(emailRegex.test(email) == false) {
+            alert('Ingresa un correo electrónico válido')
         } else {
-            $.ajax({
-                url: 'https://credigenio.mx/create/?',
-                //url:'http://localhost:1337/create/?',  
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify({
-                    monto: monto,
-                    plazo: plazo,
+            $.ajax({ 
+		        //url:'https://credigenio.mx/create/?',
+                url:'http://localhost:1337/create/?',  
+                type: 'POST', 
+                contentType: 'application/json', 
+                data: JSON.stringify({ 
+                    monto: '$1,000',
+                    plazo: '15 dias',
                     phone: phone,
                     email: email,
-                    cuenta: account,
+                    cuenta: 'Si',
                     estatus: 'null',
                     dateBirth: 'null',
-                    terms: terms,
-                    privacy: privacy,
+                    terms: 'true',
+                    privacy: 'true',
                     dateRegister: dateRegister,
                     hourRegister: dateHour
-                }),
-                error: function error(jqXhr, textStatus, errorThrown) {
-                    alert('error en el servicio');
-                },
-                success: function success(data, textStatus, jQxhr) {
-                    document.getElementById("form-lead").reset();
-                    $('#exampleModalCenter').modal({
-                        backdrop: false,
-                        show: true
-                    });
-                    $('.modal').css('background', 'rgba(0,0,0,0.5)');
-                    setTimeout(function () {
-                        $('.spinner').fadeOut('slow');
-                        fbq('track', 'Lead');
-                        setTimeout(function () {
-                            $('.fa-check-circle').fadeIn('slow');
-                            $('#msg').text('¡Hemos encontrado tu mejor opción!');
-
-                            setTimeout(function () {
-                                $('.fa-check-circle').fadeOut();
-                                $('#msg').fadeOut();
-                                setTimeout(function () {
-                                    $('.kueski').fadeIn('slow');
-                                    $('.kueski-row').css('display', 'flex');
-                                }, 500);
-                            }, 500);
-                        }, 800);
-                    }, 4000);
+                }), 
+                error: function(jqXhr, textStatus, errorThrown) { 
+                  alert('error en el servicio');  
+              }, 
+              success: function(data, textStatus, jQxhr){
+                  if(data.success) {
+                    console.log('correcto')
+                   
+                    fbq('track','Lead');
+                  } else {
+                    console.log('duplicado')
+                  };
+                  
+                  setTimeout(function(){
+                    window.location.pathname = '/step2';
+                    }, 800);
                 }
-            });
+            }); 
         }
-    });
-};
 
-$(document).ready(function () {
+
+    })
+}
+
+$(document).ready(function(){
     createLead();
-});
+})
+
